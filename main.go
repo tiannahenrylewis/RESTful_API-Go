cf7d0315a77e7de3a014f1064f1b83eae29bd0b6
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2020-01-13 14:08:22 -0500

    Update main.go
    Created Default Route and Default Handler

--- main.go
+++ main.go
@@ -6,7 +6,20 @@ import (
 	"os"
 )
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Asset not found\n"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Running API v1\n"))
+}
+
 func main() {
+	//Default Route
+	http.HandleFunc("/", rootHandler)
+
 	err := http.ListenAndServe("localhost:11111", nil)
 	if err != nil {
 		fmt.Println(err)
