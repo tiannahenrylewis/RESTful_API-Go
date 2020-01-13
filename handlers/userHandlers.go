088f9b1e3a537e9cd07662175f7509576fcc01ce
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2020-01-13 18:05:42 -0500

    Implemented POST request

--- handlers/userHandlers.go
+++ handlers/userHandlers.go
@@ -1,8 +1,12 @@
 package handlers
 
 import (
+	"gopkg.in/mgo.v2/bson"
 	"GoAPI/user"
+	"errors"
+	"io/ioutil"
 	"net/http"
+	"encoding/json"
 )
 
 
@@ -13,4 +17,41 @@ func usersGetAll(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	postBodyResponse(w, http.StatusOK, jsonResponse{"users" : users})
+}
+
+func bodyToUser(r *http.Request, u *user.User) error {
+	if r.Body == nil {
+		return errors.New("Request body is empty.")
+	}
+
+	if u == nil {
+		return errors.New("A user is required.")
+	}
+
+	bd, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(bd, u)
+}
+
+func usersPostOne(w http.ResponseWriter, r *http.Request) {
+	u := new(user.User)
+	err := bodyToUser(r, u)
+	if err != nil {
+		postError(w, http.StatusBadRequest)
+		return
+	}
+	u.ID = bson.NewObjectId()
+	err = u.Save()
+	if err != nil {
+		if err == user.ErrRecordInvalid {
+			postError(w, http.StatusBadRequest)
+		} else {
+			postError(w, http.StatusInternalServerError)
+		}
+		return
+	}
+	w.Header().Set("Location", "/users/" + u.ID.Hex())
+	w.WriteHeader(http.StatusCreated)
 }
--- handlers/usersRouter.go
+++ handlers/usersRouter.go
@@ -16,6 +16,7 @@ func UsersRouter(w http.ResponseWriter, r *http.Request) {
 			usersGetAll(w, r)
 			return
 		case http.MethodPost:
+			usersPostOne(w, r)
 			return
 		default:
 			postError(w, http.StatusMethodNotAllowed)
